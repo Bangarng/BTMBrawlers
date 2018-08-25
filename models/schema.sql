bae02f00ca296703c85b586125a875aad150df49
unknown <smarcus4@gmu.edu>
2018-08-24 20:00:48 -0400

    database creation for users

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,26 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+-- DROP DATABASE IF EXISTS exampledb;
+CREATE DATABASE users_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+use users_db;
+
+
+CREATE TABLE userInfo 
+(
+id INT NOT NULL AUTO_INCREMENT,
+userName VARCHAR(15) NOT NULL,
+pass VARCHAR(15) NOT NULL,
+charName VARCHAR(20) NOT NULL,
+attack DECIMAL(10,2) NULL,
+defense DECIMAL(10,2) NULL,
+intellect DECIMAL(10,2) NULL,
+bossOne boolean DEFAULT false,
+bossTwo boolean DEFAULT false,
+bossThree boolean DEFAULT false,
+Primary Key (id)
+
+
+INSERT INTO userInfo (userName, pass, charName, attack, defense, intellect, bossOne, bossTwo, bossThree) VALUES ('Smarcus4','sam12345','Sidekick', 15, 2, 10, false, false, false);
+
+
+
+);
