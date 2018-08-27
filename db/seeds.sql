218285c83442bc807a8bcd2337d4d56566d844c3
ijeli <legaspi.jel@gmail.com>
2018-08-27 09:19:00 -0400

    added route for api calls, hide/show for the index page, script to call function and add style

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,7 @@
+/*seed for userInfo*/
 INSERT INTO userInfo (userName, pass, charName, attack, defense, intellect, bossOne, bossTwo, bossThree) 
 VALUES ('Smarcus4','sam12345','Sidekick', 15, 2, 10, false, false, false);
 
+/*seed for item*/
 INSERT INTO items (itemName, attack, defense, intellect, lvlUnlock) 
 VALUES("PowerSword", 10, 0, 2, false);
