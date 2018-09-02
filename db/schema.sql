d5e03ea2b806c89f218a4178b5b22835957590a0
unknown <smarcus4@gmu.edu>
2018-09-02 11:48:16 -0400

    updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -34,3 +34,47 @@ lvlUnlock boolean DEFAULT false,
 Primary Key (id)
 
 );
+
+
+
+-- SAM'S SCHEMA
+
+DROP DATABASE IF EXISTS users_db;
+CREATE DATABASE users_db;
+
+use users_db;
+
+
+CREATE TABLE userInfo 
+(
+id INT NOT NULL auto_increment,
+userName VARCHAR(15) NOT NULL,
+pass VARCHAR(15) NOT NULL,
+charName VARCHAR(20) NOT NULL,
+attack DECIMAL(10,2) NULL,
+defense DECIMAL(10,2) NULL,
+intellect DECIMAL(10,2) NULL,
+itemOne VARCHAR(50) NULL,
+itemTwo VARCHAR(50) NULL,
+itemThree VARCHAR(50) NULL,
+bossOne boolean DEFAULT false,
+bossTwo boolean DEFAULT false,
+bossThree boolean DEFAULT false,
+Primary Key (id)
+
+);
+
+CREATE TABLE items
+(
+itemID INT NOT NULL AUTO_INCREMENT,
+itemName VARCHAR(50) NOT NULL,
+attack DECIMAL(10,2) NULL,
+defense DECIMAL(10,2) NULL,
+intellect DECIMAL(10,2) NULL,
+lvlUnlock boolean DEFAULT false,
+charName VARCHAR(20) NOT NULL,
+id INT null,
+Primary Key (itemID),
+FOREIGN KEY (id) REFERENCES userInfo(id)
+
+);
